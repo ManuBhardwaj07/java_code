b78a96e39767f11e66c56c8a53aa459b09649b26
Manu Bhardwaj <bhardwajkhuman@gmail.com>
2025-09-17 15:40:06 +0530

    Add files via upload

---
+++ Student2.java
@@ -0,0 +1,24 @@
+class Student2 {
+    String name;
+    int age;
+    static int maxAge = 20;
+  
+    public static void main(String[] args) {
+        Student2 x = new Student2();
+        
+        x.name = "Rajan";
+        x.age = 12;
+
+        Student2 y = new Student2();
+
+        y.name = "Gopal";
+        y.age = 14;
+
+        x.showInfo();
+        y.showInfo();
+    }
+
+    void showInfo() {
+        System.out.println(name + " - " + age + " - " + maxAge);
+    }
+}

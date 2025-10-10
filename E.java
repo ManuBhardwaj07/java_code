bc489dd25d94c4a5543fca2e05154fd5098f820d
Manu Bhardwaj <bhardwajkhuman@gmail.com>
2025-10-10 22:28:50 +0530

    Add files via upload

--- E.java
+++ E.java
@@ -1,11 +1,15 @@
-class E {
-   public static void main (String[] args ){
-    E1.pro();
+final class E1 {
+    static int y = 2;
 
-   } 
+    static void pro() {
+        System.out.println("Hello");
+    }
 }
-class E1{
-    static void pro (){
-        System.out.println("Manu Bhardwaj");
+
+class E {
+    public static void main(String[] args) {
+        System.out.println(E1.y);
+
+        E1.pro();
     }
 }
--- F.java
+++ F.java
@@ -1,12 +1,17 @@
-class F{
-    public static void main(String[] args) {
-        F1.info();
+final class F1 {
+    int y = 99;
+
+    void pro() {
+        System.out.println("Hi");
     }
-    
 }
-class F1 {
-    static void info(){
-        System.out.println("Hello World ");
-    
+
+class F {
+    public static void main(String[] args) {
+        F1 a = new F1();
+
+        System.out.println(a.y);
+
+        a.pro();
     }
 }
